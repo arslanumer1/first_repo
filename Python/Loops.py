7fcc3778b1aeea92a84b878523947df1bdef0c9f
Arslan <arslnumer@gmail.com>
2025-10-14 14:27:56 -0500

    Learn condition and loops in python

---
+++ Python/Loops.py
@@ -0,0 +1,65 @@
+# cars = [ "Ford", "BMW", "Volvo", "Audi", "Mercedes", "Tesla"]
+#
+# print(cars[0])
+# print(cars[1])
+# print(cars[2])
+# print(cars[3])
+#
+# for car in cars:
+#     print(car)
+from operator import truediv
+
+# name = "Alice"
+#
+# for letter in name:
+#     print(letter)
+
+# for i in range(10):
+#     print(i)
+
+# for i in range(5, 10):
+#     print(i)
+
+# for i in range(5):
+#     print("Hello")
+
+# for i in range(5):
+#     if i !=3:
+#         print(i)
+#     else:
+#         print ("skipped")
+
+# for i in range(5):
+#     if i == 2:
+#         break
+#     print(i)
+#
+# print("End of program")
+
+# for i in range(5):
+#     if i == 2:
+#         continue
+#     print(i)
+#
+# print("End of program")
+
+
+# while True:
+#     print("Hello")
+#     if input() == "q":
+#         break
+
+# i = 0
+# while i < 5:
+#     print(i)
+#     i += 1
+
+# i= 0
+# result = True
+#
+# while result:
+#     print(i)
+#     i += 1
+#     if i == 5:
+#         result = False
+
---
+++ Python/condition.py
@@ -0,0 +1,35 @@
+# answer = int(input("What is your age?"))
+#
+# if answer > 5:
+#     print("Less then 5")
+#
+# print("End of program")
+answer = int(input("What is your age? "))
+
+# if answer >= 5 and answer <=10:
+#     print("Not equal 5")
+#
+# print("End of program")
+
+# # and
+# true and true = true
+# true and false = false
+# false and true = false
+# false and false = false
+
+# or
+# true or true = true
+# true or false = true
+# false or true = true
+# false or false = false
+
+if answer < 5:
+    print("Less then 5")
+elif answer < 10:
+    print("Less then 10")
+elif answer < 15:
+    print("Less then 15")
+else:
+    print("Greater then 15")
+
+Print("End of program")
