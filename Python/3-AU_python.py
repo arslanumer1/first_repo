d179c1a6a860469e84df9a5cb17ece6700e63448
Arslan <arslnumer@gmail.com>
2025-10-31 09:08:09 -0500

    Add Playwright script for automated multi-browser login simulation

---
+++ Python/3-AU_python.py
@@ -0,0 +1,12 @@
+from playwright.sync_api import sync_playwright
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False)
+    page = browser.new_page()
+    page.goto("https://www.saucedemo.com/")
+    page.locator("#user-name").fill("standard_user")
+    page.locator("#password").fill("secret_sauce")
+    page.locator("#login-button").click()
+    page.screenshot(path="google.png")
+    browser.close()
+
