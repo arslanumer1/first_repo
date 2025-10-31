169afb59de58b3765369a90a321f809f5ac4b2d8
Arslan <arslnumer@gmail.com>
2025-10-31 10:35:49 -0500

    Add Python script demonstrating basic usage of classes and objects with Animal and Degree examples

--- Python/4-Classes.py
+++ Python/4-Classes.py
@@ -1,15 +1,17 @@
 class Animal:
-    def __init__(self,legs, tail=True, name="Animal"):
+    def __init__(self, legs, tail=True, name="name"):
         self.legs = legs
         self.tail = True
-        self.name = Animal
+        self.name = name
 
     def run(self):
         print(f"My {self.name} can run")
     def eat(self):
-        print(f"My {Animal} can eat")
+        print(f"My {self.name} can eat")
 
-cat = Animal(4, True, "cat")
+
+cat = Animal(4, True, "Cat")
+
 cat.run()
 cat.eat()
 
@@ -30,3 +32,18 @@ class Degree:
 
 Arslan = Degree("Arslan", "Balkus", 5, 4)
 print(Arslan)
+
+
+class UnderGrad(Degree):
+    University = "UTD"
+
+    def __str__(self):
+        return (
+            f"{self.name} is in {self.num_of_years}th year and in {self.University} "
+            f"with {self.num_of_publications} publications"
+            f" and under the supervision of {self.advisor}"
+        )
+
+
+Umair = UnderGrad("Umair", "Stefan", 5, 4)
+print(Umair)
